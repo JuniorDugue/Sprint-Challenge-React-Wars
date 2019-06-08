941ac88d8fac83bf71c2646cf85ddff0177ff8c8
Junior Dugue <jrdugue@gmail.com>
2019-06-08 13:14:31 -0400

    added a few more data for the other characters

--- starwars/src/components/StarWarsCharacters.js
+++ starwars/src/components/StarWarsCharacters.js
@@ -5,7 +5,18 @@ import "./StarWars.scss";
 const StarWarsCharacters = (props) => {
   return(
     <div className="starWarsContainer">
-      <p>{props.character.name}</p>
+      <h3>{props.character.name}</h3>
+      <p>
+        <strong>Species:</strong>{props.character.species}
+      </p>
+      <p>
+        <strong>Gender:</strong>
+        {props.character.gender}
+      </p>
+      <p>
+        <strong>Height:</strong>
+        {props.character.height}
+      </p>
     </div>
   )
 }
